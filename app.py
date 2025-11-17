bc062a183c453cd8da3b49dac73e88fbedfb1f53
ZoeSmartbees <zoe@smart-bees.fr>
2025-11-17 11:51:07 +0100

    suppression de gtag, utilisation uniquement de GTM

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/next")
+def next_page():
+    return render_template("next.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+# le site se trouve sur http://dev.monsite.local:5000/
